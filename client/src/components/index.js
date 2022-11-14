06c48c5525a650c073a52be3ece359037cdb858f
Jamie Reardon <jamie.reardon91@gmail.com>
2022-11-14 18:27:43 +0000

    update components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,13 +1,23 @@
-export { default as withContext } from './Context';
+import withContext from './Context';
+import Header from './Header';
+import UserSignUp from './UserSignUp';
+import UserSignIn from './UserSignIn';
+import UserSignOut from './UserSignOut';
+import Courses from './Courses';
+import CourseDetail from './CourseDetail';
+import CreateCourse from './CreateCourse';
+import UpdateCourse from './UpdateCourse';
+
+export const HeaderWithContext = withContext(Header);
+export const UserSignUpWithContext = withContext(UserSignUp);
+export const UserSignInWithContext = withContext(UserSignIn);
+export const UserSignOutWithContext = withContext(UserSignOut);
+export const CoursesWithContext = withContext(Courses);
+export const CourseDetailWithContext = withContext(CourseDetail);
+export const CreateCourseWithContext = withContext(CreateCourse);
+export const UpdateCourseWithContext = withContext(UpdateCourse);
+
 export { default as PrivateRoute } from './PrivateRoute';
-export { default as Header } from './Header';
-export { default as Courses } from './Courses';
-export { default as CourseDetail } from './CourseDetail';
-export { default as CreateCourse } from './CreateCourse';
-export { default as UpdateCourse } from './UpdateCourse';
-export { default as UserSignIn } from './UserSignIn';
-export { default as UserSignUp } from './UserSignUp';
-export { default as UserSignOut } from './UserSignOut';
 export { default as NotFound } from './NotFound';
 export { default as Forbidden } from './Forbidden';
 export { default as UnhandledError } from './UnhandledError';
